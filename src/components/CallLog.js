fa428921c4cfdd9ef4914f072f384f4c904bece8
Alexander Kachkaev <alexander@kachkaev.ru>
2017-09-02 16:20:09 +0100

    Allow clicks on past calls only when call status is idle

--- src/components/CallLog.js
+++ src/components/CallLog.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 import styled from 'styled-components';
 import Paper from 'material-ui/Paper';
-import { compose, withHandlers } from 'recompose';
+import { compose, getContext, withHandlers, withPropsOnChange } from 'recompose';
+import { CALL_STATUS_IDLE } from 'react-sip';
 import { connect } from 'react-redux';
 
 import List, { ListItem, ListItemText, ListSubheader } from 'material-ui/List';
@@ -14,11 +16,16 @@ const Wrapper = styled(Paper).attrs({
   display: flex;
 `;
 
-const DialLog = ({ entries, onListItemClick }) =>
+const DialLog = ({ entries, onListItemClick, allowListItemClicks }) =>
   (<Wrapper>
     <List dense subheader={<ListSubheader>Your Call Log</ListSubheader>}>
       {_.map(entries, (entry, i) =>
-        (<ListItem button key={i} data-phonenumber={entry.phoneNumber} onClick={onListItemClick}>
+        (<ListItem
+          button={allowListItemClicks}
+          key={i}
+          data-phonenumber={entry.phoneNumber}
+          onClick={onListItemClick}
+        >
           <ListItemText primary={entry.phoneNumber} />
         </ListItem>),
       )}
@@ -26,6 +33,12 @@ const DialLog = ({ entries, onListItemClick }) =>
   </Wrapper>);
 
 export default compose(
+  getContext({
+    callStatus: PropTypes.string,
+  }),
+  withPropsOnChange(['callStatus'], ({ callStatus }) => ({
+    allowListItemClicks: callStatus === CALL_STATUS_IDLE,
+  })),
   connect((state) => state.callLog),
   withHandlers({
     onListItemClick: ({ dispatch }) => (e) => {
