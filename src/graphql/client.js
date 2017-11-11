10e1bb98ed1e0c2a55dfbb796430abcb6de8bddf
Alexander Kachkaev <alexander@kachkaev.ru>
2017-11-11 19:04:51 +0000

    Add token header from cookie

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,9 +1,21 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { parse } from 'cookie';
+
+const authLink = setContext((_, { headers }) => {
+  const { token } = parse(document.cookie);
+  return {
+    headers: {
+      ...headers,
+      token,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: new HttpLink(),
+  link: authLink.concat(new HttpLink()),
   cache: new InMemoryCache(),
 });
 
