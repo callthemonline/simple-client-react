dc14fdb46a2dce99aa6954b3819d1fb26c5142f3
Denis Nikulin <denisnikulin@gmail.com>
2018-02-03 19:54:45 +0000

    language detector rename

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,17 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-import LngDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
 
-const lngDetector = new LngDetector();
-lngDetector.addDetector({
+const languageDetector = new LanguageDetector();
+languageDetector.addDetector({
   name: 'languageByDomain',
   lookup: (options) => options.hostname.match(/\.im$/i) ? 'ru' : 'en',
 });
 
 i18n
   .use(Backend)
-  .use(lngDetector)
+  .use(languageDetector)
   .use(reactI18nextModule)
   .init({
     detection: {
